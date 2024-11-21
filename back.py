a22f5b1539b29390aee1b0c58486ca385ea1b371
ethanbsung <ebsung@umich.edu>
2024-11-21 16:45:05 -0500

    First

---
+++ back.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from enum import Enum
+import random
+
+class Ranks(Enum):
+    TWO = "Two"
+    THREE = "Three"
+    FOUR = "Four"
+    FIVE = "Five"
+    SIX = "Six"
+    SEVEN = "Seven"
+    EIGHT = "Eight"
+    NINE = "Nine"
+    TEN = "Ten"
+    JACK = "Jack"
+    QUEEN = "Queen"
+    KING = "King"
+    ACE = "Ace"
+
+class Suits(Enum):
+    SPADES = "Spades"
+    CLUBS = "Clubs"
+    DIAMONDS = "Diamonds"
+    HEARTS = "Hearts" 
+
+
+class Player:
+    def __init__(self, score, balance, hand):
+        self.score = score
+        self.balance = balance
+        self.hand = hand
+
+class Dealer:
+    def __init__(self, hand):
+        self.hand = hand
+
+class Game:
+    deck = [(rank, suit) for rank in Ranks for suit in Suits]
+    random.shuffle(deck)
+    for card in deck:
+        print(f"{card[0].value} of {card[1].value}")
+
+    player = Player(0, 0, [(Ranks.TWO, Suits.CLUBS), (Ranks.TWO, Suits.HEARTS)])
+    for card in player.hand:
+        print(f"{card[0].value} of {card[1].value}")
+
+    dealer = Dealer([(Ranks.TWO, Suits.DIAMONDS), (Ranks.TWO, Suits.SPADES)])
+    for card in dealer.hand:
+        print(f"{card[0].value} of {card[1].value}")
+    
