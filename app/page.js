4ce11df01cbbcbf2fc5af60a96fac2978cbdb91e
ethanbsung <ebsung@umich.edu>
2024-11-22 17:50:29 -0500

    Deal button

--- app/page.js
+++ app/page.js
@@ -11,7 +11,8 @@ import { useState } from 'react';
 
   export default function HomePage() {
     const [balance, setBalance] = useState(1000);
-    
+    const [dealerScore, setDealerScore] = useState(0);
+    const [playerScore, setPlayerScore] = useState(0);
     function HandleClick(action) {
       console.log(`Button clicked: ${action}`);
     }
@@ -20,10 +21,14 @@ import { useState } from 'react';
       <div>
         <Header title="Blackjack" />
         <p>Balance: ${balance}</p>
+        <p>Dealer: {dealerScore}</p>
+        <p>Player: {playerScore}</p>
+  
         <button onClick={() => HandleClick('Hit')}>Hit</button>
         <button onClick={() => HandleClick('Stand')}>Stand</button>
         <button onClick={() => HandleClick('Double')}>Double</button>
         <button onClick={() => HandleClick('Split')}>Split</button>
+        <button onClick={() => HandleClick('Deal')}>Deal</button>
       </div>
     );
   }
